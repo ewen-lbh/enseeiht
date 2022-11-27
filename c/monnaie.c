bde82eafd3958523c3313a77b79cad57d5672f5b
Ewen Le Bihan <hey@ewen.works>
2022-11-27 21:28:30 +0100

    c: finish bilan 2

--- c/monnaie.c
+++ c/monnaie.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <assert.h>
 #define TAILLE_PORTE_MONNAIE 5
 
@@ -10,12 +11,12 @@ struct monnaie {
 };
 
 
-struct monnaie* init_monnaie(int valeur, char devise) {
-    if (valeur < 0) return;
+struct monnaie init_monnaie(int valeur, char devise) {
+    if (valeur < 0) exit(1);
 
-    struct monnaie *m = NULL;
-    m->valeur = valeur;
-    m->devise = devise;
+    struct monnaie m = {valeur, devise};
+    // m->valeur = valeur;
+    // // m->devise = devise;
     return m;
 }
 
@@ -27,33 +28,49 @@ bool add(struct monnaie* m1, struct monnaie* m2) {
 }
 
 void test() {
-    struct monnaie* usd3 = init_monnaie(3, '$');
-    struct monnaie* eur8 = init_monnaie(8, 'e');
-    struct monnaie* eur10 = init_monnaie(10, 'e');
-    assert(3 == usd3->valeur);
-    assert(false == add(usd3, eur8));
-    assert(true == add(eur8, eur10));
-    assert(18 == eur10->valeur);
-    assert(8 == eur8->valeur);
-    assert('e' == eur10->devise);
+    struct monnaie usd3 = init_monnaie(3, '$');
+    struct monnaie eur8 = init_monnaie(8, 'e');
+    struct monnaie eur10 = init_monnaie(10, 'e');
+    assert(3 == usd3.valeur);
+    assert(false == add(&usd3, &eur8));
+    assert(true == add(&eur8, &eur10));
+    assert(18 == eur10.valeur);
+    assert(8 == eur8.valeur);
+    assert('e' == eur10.devise);
+}
+
+void print_porte_monnaie(struct monnaie* porte_monnaie) {
+    for (int i = 0; i < TAILLE_PORTE_MONNAIE; i++) {
+        printf("· %d%c\n", porte_monnaie[i].valeur, porte_monnaie[i].devise);
+    }
 }
 
 int main() {
+    test();
+
     struct monnaie porte_monnaie[TAILLE_PORTE_MONNAIE];
     struct monnaie somme = init_monnaie(0, ' ');
     
     for (int i = 0; i < TAILLE_PORTE_MONNAIE; i++) {
         int valeur;
         char devise;
-        scanf("%d\n", valeur);
-        scanf("%c\n", devise);
+        scanf(" %d", &valeur);
+        scanf(" %c", &devise);
         porte_monnaie[i] = init_monnaie(valeur, devise);
-        if (i == 0) {
-            somme = init_monnaie(0, devise);
-        } else {
-            add(somme, porte_monnaie[i]);
+        print_porte_monnaie(porte_monnaie);
+    }
+
+    printf("\n Somme des monnaies en? ");
+    char devise;
+    scanf(" %c", &devise);
+    somme = init_monnaie(0, devise);
+    for (int i = 0; i < TAILLE_PORTE_MONNAIE; i++) {
+        if (porte_monnaie[i].devise == devise) {
+            add(&porte_monnaie[i], &somme);
         }
     }
+
+    printf("Somme: %d %c", somme.valeur, somme.devise);
 
     return 0;
 }
