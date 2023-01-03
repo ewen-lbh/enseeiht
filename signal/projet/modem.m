5caeca60203cc2f24c1d3046dfe8b167e65a5356
Ewen Le Bihan <hey@ewen.works>
2023-01-03 18:17:51 +0100

    signal: projet

--- signal/projet/modem.m
+++ signal/projet/modem.m
@@ -5,7 +5,7 @@ Fe = 48000;
 Te = 1/Fe;
 Ts = 1/300; % 1 / débit en bits/sec
 Ns = floor(Ts/Te);
-Nbits = 10;
+Nbits = 100;
 phi0=rand*2*pi;
 phi1=rand*2*pi;
 layout = tiledlayout(3, 3)
@@ -84,9 +84,9 @@ legend("Théorique", "Expérimentale")
 nexttile(layout)
 snr = 200;
 puissance_module = mean(abs(module).^2);
-sigma = 1/(puissance_module * 10^(snr/10));
-noise = sigma * randn(1, length(module));
-module_bruite = module + noise;
+sigma = sqrt(puissance_module/10^(snr/10));
+bruit = sigma * randn(1, length(module));
+module_bruite = module + bruit;
 plot(module_bruite)
 xlabel("Temps [s]")
 ylabel("Amplitude")
@@ -94,23 +94,35 @@ title("Signal bruité")
 
 % 5
 f = linspace(-Fe/2, Fe/2, length(module_bruite));
-
 ordre_2 = 61
 t_ordre_2 = -(ordre_2 - 1) / 2 * Te : Te : (ordre_2 - 1)/2 * Te
 fc = (frequence_0 + frequence_1) / 2
-lpf = 2 * fc * sinc(2 * fc * t_ordre_2)
-recu_1 = filter(t_ordre_2, 1, module_bruite)
+h = 2 * fc * sinc(2 * fc * t_ordre_2)
+H = fft(h, 4096)
+
+% recu_1 = lowpass(module_bruite, (fc + 20)/Fe)
+recu_1 = filter(h, 1, module_bruite)
+%recu_0 = 
+
 %b0 = 2*pi*(frequence_1+tolerance)/Fe
 %recu_1 = filter(b0, [1 1-b0], module_bruite)
 
 % recu_0 = abs(ifft(fftshift(fft(module_bruite)) .* (f > frequence_0 - tolerance)));
 % recu_1 = abs(ifft(fftshift(fft(module_bruite)) .* (f < frequence_1 + tolerance)));
 nexttile(layout)
-% plot(recu_0)
-% hold on
+%plot(recu_1)
+hold on
 plot(recu_1)
 xlabel("Temps [s]")
 ylabel("Amplitude")
 title("Signal démodulé")
-legend("0", "1")
+legend("1", "0")
 
+
+%5-4
+%Signal modulé en fréquence
+%densité spectrale puissance
+
+dsp2=fft(autocorr(nrz));
+plot(dsp2);
+hold on;
