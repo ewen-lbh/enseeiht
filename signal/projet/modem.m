8da97e5217a38811c93b5e61c870e96bf35ea76a
Ewen Le Bihan <hey@ewen.works>
2023-01-10 14:50:40 +0100

    signal: projet: FILTRAGE MAAAAAAAAAAARCHE

--- signal/projet/modem.m
+++ signal/projet/modem.m
@@ -5,7 +5,7 @@ Fe = 48000;
 Te = 1/Fe;
 Ts = 1/300; % 1 / débit en bits/sec
 Ns = floor(Ts/Te);
-Nbits = 100;
+Nbits = 10;
 phi0=rand*2*pi;
 phi1=rand*2*pi;
 layout = tiledlayout(3, 3)
@@ -38,21 +38,14 @@ title("Densité spectrale de puissance")
 
 hold on;
 
-% Attention, la définition Matlab de sinc prend déjà en compte pi. Voir section "More about" de https://fr.mathworks.com/help/signal/ref/sinc.html
-% De plus la fonction dirac de matlab est la fonction 'continue' (donc vaut inf en 0), il ne faut donc pas l'utiliser de cette façon.
-% Pour dirac tu peux faire quelque chose comme :
-% dsp2(find(f==0)) = dsp2(find(f==0)) + 1
-% ce qui change uniquement la valeur de dsp2 où f==0
-dsp2=0.25*Ts*(sinc(pi*f*Ts).^2)+0.25*dirac(f);
+dsp2 = 0.25*Ts*(sinc(f*Ts).^2);
+dsp2(find(f==0)) = dsp2(find(f==0)) + 0.25
 semilogy(f, dsp2, 'r')
 legend("Pratique", "Théorique")
 
 % 3.2.1
 nexttile(layout)
-% t est mal défini : Tu commences à 1s, et tu termines à Nbits*Ts=100*0.003333=0.3333 secondes
-% Il faut changer les bornes du linspace (attention à bien avoir des points
-% tous les Te) ou alors définir simplement t comme t=0:Te:(Nbits*Ns-1)*Te
-t = linspace(1, Nbits*Ts, Nbits*Ns);
+t=0:Te:(Nbits*Ns-1)*Te;
 size(t)
 size(nrz)
 frequence_0 = 6000; % 1180;
@@ -71,10 +64,8 @@ nexttile(layout)
 
 % 3.2.3
 
-dsp4=abs(fft(module)).^2;
+dsp4=abs(fftshift(fft(module))).^2;
 f=linspace(-Fe/2, Fe/2, length(dsp4));
-% Attention, fft calcule la transformée de Fourier entre 0 et Fe, 
-% pas -Fe/2 et Fe/2
 semilogy(f, dsp4)
 xlabel("Fréquence [Hz]")
 ylabel("Amplitude")
@@ -84,10 +75,8 @@ title("Densité spectrale de puissance")
 hold on;
 
 % 3.2.4
-dsp3=pwelch(module, [], [], [], Fe, 'twosided');
+dsp3=fftshift(pwelch(module, [], [], [], Fe, 'twosided'));
 f=linspace(-Fe/2, Fe/2, length(dsp3));
-% Attention, welch calcule la transformée de Fourier entre 0 et Fe, 
-% pas -Fe/2 et Fe/2
 semilogy(f, dsp3)
 legend("Théorique", "Expérimentale")
 
@@ -112,22 +101,22 @@ fc = (frequence_0 + frequence_1) / 2
 % A l'intérieur du sinc c'est bon car tu fais apparaitre Te=1/Fe dans t_ordre_2
 % Mais pour le module de h ce n'est pas bon (d'ailleurs h est sensé être
 % adimensionnel et dans ton cas il est exprimé en Hz)
-h = 2 * fc * sinc(2 * fc * t_ordre_2)
-H = fft(h, 4096)
 
-% recu_1 = lowpass(module_bruite, (fc + 20)/Fe)
-recu_1 = filter(h, 1, module_bruite)
-%recu_0 = 
+fc2=fc
 
-%b0 = 2*pi*(frequence_1+tolerance)/Fe
-%recu_1 = filter(b0, [1 1-b0], module_bruite)
+h_haut = 2 * (fc2 / Fe) * sinc(2 * fc2 * t_ordre_2)
+H_haut = fft(h_haut, 4096)
 
-% recu_0 = abs(ifft(fftshift(fft(module_bruite)) .* (f > frequence_0 - tolerance)));
-% recu_1 = abs(ifft(fftshift(fft(module_bruite)) .* (f < frequence_1 + tolerance)));
+h_bas = -h_haut
+h_bas(find(t_ordre_2==0)) = h_bas(find(t_ordre_2==0)) + 1
+
+recu_1 = filter(h_haut, 1, module_bruite)
+recu_0 = filter(h_bas, 1, module_bruite)
+
 nexttile(layout)
-%plot(recu_1)
 hold on
 plot(recu_1)
+plot(recu_0)
 xlabel("Temps [s]")
 ylabel("Amplitude")
 title("Signal démodulé")
@@ -138,6 +127,7 @@ legend("1", "0")
 %Signal modulé en fréquence
 %densité spectrale puissance
 
+nexttile(layout)
 dsp2=fft(autocorr(nrz));
 plot(dsp2);
 hold on;
