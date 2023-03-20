8ff8e7e110a466d1fd4966ba1b8d6d9dadae1594
Puy Florent <fpuy@ewok.enseeiht.fr>
2023-03-20 18:21:19 +0100

    up

--- télécommunications/proj/Partie4.m
+++ télécommunications/proj/Partie4.m
@@ -1,5 +1,6 @@
 % Chaine 1
-
+clear all;
+close all;
 n=1;
 
 Fe = 24000;
@@ -8,49 +9,71 @@ Rb = 3000;
 Tb = 1/Rb;
 
 Ts = n*Tb;
-Nbits = 10000;
+Nbits = 100000;
 Ns = floor(Ts/Te);
+Eb_N0_dB = 0:0.5:8;
+Eb_N0_lineaire = 10.^(Eb_N0_dB/10);
+tebs = zeros(0, length(Eb_N0_dB))
 
-bits_non_map = randi([0, 1], 1, Nbits); %Nbits
-bits_non_map2 = randi([0, 1], 1, 15*Nbits);
+for index = 1:length(Eb_N0_dB)
+    
+    bits_non_map = randi([0, 1], 1, Nbits);
+    
+    signal_1 = 2*bits_non_map - 1;
+    signal_1_map = kron(signal_1, [1 zeros(1, Ns-1)]);
+    h = ones(1,Ns);
+    hr = ones(1,Ns);
+    x = filter(h,1,signal_1_map);
+    M = 2^n;
+    n_0 = 8;
+    
+    P = mean(abs(x).^2);
+    sigma = sqrt((P * Ns) ./ (2 * log2(M) .* (Eb_N0_lineaire(index))));
+    bruit = sigma .* randn(1, length(signal_1_map));
+    r = x+bruit;
+    z = filter(hr,1,r);
+    unmapped_ech = z(n_0:Ns:end);
 
-signal_1 = 2*bits_non_map - ones(1, length(bits_non_map));
-signal_1_map = kron(signal_1, [1 zeros(1, Ns-1)]);
-f = linspace(-Te/2, Te/2, Ns);
-h_2 = ones(1, Ns); 
+    unmapped = (sign(unmapped_ech ) + 1)/ 2;
+    tebs_theorique(index) = qfunc(sqrt(2 .* Eb_N0_lineaire(index)));
+    tebs(index) = length(find((unmapped ~= bits_non_map)))/length(bits_non_map)
+end
 
 
-figure;
-plot(signal_1_map)
-xlabel("temps [s]")
-ylabel("m_i(t)")
-title("Signal aléatoire")
+figure
+semilogy(Eb_N0_dB, tebs,'b')
+hold on
+semilogy(Eb_N0_dB, tebs_theorique,'r')
 
-figure;
-%x=filter(signal_1, 1, ones(1,Ns));
-x=filter(h_2,1 , signal_1_map);
-t = 0:Te:(length(x)-1)*Te;
-plot(t, x)
-title("(1) Signal filtré")
 
-figure;
+tebs = zeros(0, length(Eb_N0_dB))
 
-dsp_1 = fftshift(pwelch(x, [], [], [], Fe, 'twosided'));
-f=linspace(-Fe/2, Fe/2, length(dsp_1));
-%semilogy(f, dsp_1)
-%xlabel("fréquence [Hz]")
-%ylabel("Densité spectrale de puissance")
-%title("(1) Densité spectrale de puissance théorique")
+for index = 1:length(Eb_N0_dB)
+    
+    bits_non_map = randi([0, 1], 1, Nbits);
+    
+    signal_1 = 2*bits_non_map - 1;
+    signal_1_map = kron(signal_1, [1 zeros(1, Ns-1)]);
+    h = ones(1,Ns);
+    hr = ones(1,Ns/2);
+    x = filter(h,1,signal_1_map);
+    M = 2^n;
+    n_0 = 8;
+    
+    P = mean(abs(x).^2);
+    sigma = sqrt((P * Ns) ./ (2 * log2(M) .* (Eb_N0_lineaire(index))));
+    bruit = sigma .* randn(1, length(signal_1_map));
+    r = x+bruit;
+    z = filter(hr,1,r);
+    unmapped_ech = z(n_0:Ns:end);
 
-h = ones(1, Ns); 
+    unmapped = (sign(unmapped_ech ) + 1)/ 2;
+    tebs_theorique(index) = qfunc(sqrt(Eb_N0_lineaire(index)));
+    tebs(index) = length(find((unmapped ~= bits_non_map)))/length(bits_non_map)
+end
 
-filtered = filter(h, 1, signal_1_map)
 
-P = (mean(abs(filtered).^2)
-M = 2^n
-Eb = P / Nbits
-N0 = Nbits / B
-
-sigma = sqrt(P * Ns) / (2 * log2(M) * (Eb / N0)))
-
-bruit = sigma * randn(1, length(filtered))
+figure
+semilogy(Eb_N0_dB, tebs,'b')
+hold on
+semilogy(Eb_N0_dB, tebs_theorique,'r')
