94fabcb7e91ca68d95b13b8ea09dd444beb18d1e
GautRrr <115937566+GautRrr@users.noreply.github.com>
2023-04-19 20:15:57 +0200

    correction --'

--- calcul/projet/ReconstructionImage.m
+++ calcul/projet/ReconstructionImage.m
@@ -85,9 +85,87 @@ puiss = 1;
 
 %%eig
 
-%VP(I,eps,maxit,search_space,percentage,puiss,"eig",U, S, V)
-%VP(I,eps,maxit,search_space,percentage,puiss,"power method",U, S, V)
-%VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_0",U, S, V)
-VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_1",U, S, V)
-VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_2",U, S, V)
-VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_3",U, S, V)
+VP(I,eps,maxit,search_space,percentage,puiss,"eig")
+%VP(I,eps,maxit,search_space,percentage,puiss,"power method")
+VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_0")
+VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_1")
+VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_2")
+VP(I,eps,maxit,search_space,percentage,puiss,"sub_space_3")
+
+%%Couleurs
+
+I=imread('minirgb.jpg');
+%imshow(I)
+Im_final = zeros(size(I));
+I=double(I);
+
+[q,p]=size(I);
+size(I)
+taillechoisi=1
+for color=1:3
+
+M=I(:,:,color)*I(:,:,color)';
+[q, p] = size(I(:,:,color))
+[U,D]=eig(M);
+D=diag(D);
+[D,perm]=sort(D,'descend');
+U=U(:,perm);
+D_200 = D(1:taillechoisi);
+U_200 = U(:, 1:taillechoisi);
+% calcul des valeurs singulières
+Sigma200 = sqrt(D_200);
+Sigma200 = diag(Sigma200);
+% calcul de l'autre ensemble de vecteurs
+V_200 = zeros(p,taillechoisi);
+tic
+for i=1:taillechoisi
+V_200(:,i) = (1/Sigma200(i,i))*(I(:,:,color)'*U_200(:,i));
+end
+toc
+size(U_200)
+size(V_200)
+% calcul des meilleures approximations de rang faible
+% vecteur pour stocker la différence entre l'image et l'image reconstuite
+inter = 1:40:(taillechoisi+40);
+inter(end) = taillechoisi;
+differenceSVD = zeros(size(inter,2), 1);
+
+ti = 0;
+Im_k = zeros(q,p);
+Im_k = U_200(:, 1:taillechoisi)*Sigma200(1:taillechoisi, 1:taillechoisi)*V_200(:, 1:taillechoisi)';
+if color == 1
+    Im_red = Im_k;
+elseif color == 2
+    Im_green = Im_k;
+else
+    Im_blue = Im_k;
+end
+Im_final(:,:,color) = Im_k;
+colormap('gray')
+imagesc(Im_k)
+
+% Figure des différences entre image réelle et image reconstruite
+ti = ti+1;
+figure(ti)
+hold on
+plot(inter, differenceSVD, 'rx')
+ylabel('RMSE')
+xlabel('rank k')
+pause
+
+end
+
+figure(1)
+
+
+Im_final= cat(3,Im_red,Im_green,Im_blue);
+%subplot red green blue final (4 images)
+subplot(2,2,1)
+colormap('gray')
+imagesc(Im_red)
+subplot(2,2,2)
+imagesc(Im_green)
+subplot(2,2,3)
+imagesc(Im_blue)
+subplot(2,2,4)
+imshow(Im_final)
