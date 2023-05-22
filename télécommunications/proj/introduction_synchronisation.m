992b7f54a62a9f034b84c3f293d406bd3a3f87e7
Ewen Le Bihan <hey@ewen.works>
2023-05-22 17:52:56 +0200

    telecom: projet: codage par transition [end]

--- télécommunications/proj/introduction_synchronisation.m
+++ télécommunications/proj/introduction_synchronisation.m
@@ -1,6 +1,8 @@
 close all
 clear all
 
+% 3 fichiers pour cette partie
+
 n=1;
 % TODO METTRE PHI=40 et PHI=100 SUR LA MEME FIGURE
 
@@ -15,14 +17,25 @@ Ts = n*Tb;
 Nbits = 100000;
 Ns = floor(Ts/Te);
 
+
 % BITS
 bits = randi([0, 1], 1, Nbits);
 
 % MAPPING
 apres_mapping = 2*bits - 1;
 
+% CODAGE PAR TRANSITION
+apres_codage = coder_par_transition(apres_mapping);
+
+figure
+plot(apres_mapping(1:40))
+hold on
+plot(apres_codage(1:40))
+legend('Apres mapping', 'Apres codage par transition')
+
+
 % SURECHANTILLONAGE
-apres_surechantillonage = kron(apres_mapping, [1 zeros(1, Ns-1)]);
+apres_surechantillonage = kron(apres_codage, [1 zeros(1, Ns-1)]);
 
 % FILTRAGE
 f = linspace(-Te/2, Te/2, Ns);
@@ -70,10 +83,12 @@ for index = 1:length(Eb_N0_dBs)
         
         % DECISION
         apres_decision = sign(real(apres_correction_erreur_phase));
-        
+
+        % DECODAGE
+        apres_decodage = decoder_par_transition(apres_decision);
         
         % DEMAPPING
-        apres_demapping = (apres_decision+1)/2;
+        apres_demapping = (apres_decodage+1)/2;
         
         % TEB
         if erreur_phase == 0
@@ -105,6 +120,23 @@ ylabel("TEB")
 % Figures à faire
 % (comparaison TEBs + constellation) avec phi 0 et 40 et 100
 
+function apres_codage = coder_par_transition(apres_mapping)
+    apres_codage = zeros(1, length(apres_mapping));
+    c_0 = 1;
+    apres_codage(1) = c_0 * apres_mapping(1);
+    for i = 2:length(apres_mapping)
+        apres_codage(i) = apres_codage(i-1) * apres_mapping(i);
+    end
+end
+
+function apres_decodage = decoder_par_transition(apres_decision)
+    apres_decodage = zeros(1, length(apres_decision));
+    c_0 = 1;
+    apres_decodage(1) = apres_decision(1) / c_0;
+    for i = 2:length(apres_decodage)
+        apres_decodage(i) = apres_decision(i) / apres_decision(i-1);
+    end
+end
 
 function bruite = bruitage(signal, Eb_N0_dB, Ns, M)
     Eb_N0_lineaire = 10.^(Eb_N0_dB/10);
